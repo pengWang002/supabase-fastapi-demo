7fbbcce18b12705070a56be25ea32f7370861293
pengWang002 <3363461465@qq.com>
2025-11-25 15:50:57 +0800

    chore: point frontend to https backend

--- frontend/config.js
+++ frontend/config.js
@@ -3,5 +3,5 @@ window.CONFIG = {
   supabaseUrl: "https://txadrpfyutdqodlmifqo.supabase.co",
   supabaseAnonKey:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4YWRycGZ5dXRkcW9kbG1pZnFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5NjY5NDAsImV4cCI6MjA3OTU0Mjk0MH0.7VEKaDmoL0A3hZ4Hy-zSc5XCOhXwqn_G4Tvz4B_DhK8",
-  backendUrl: "http://47.94.149.54:8000",
+  backendUrl: "https://api.wwwppp.online",
 };
